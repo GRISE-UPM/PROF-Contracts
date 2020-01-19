af380cfe74291d60f2e0f690fd5633eace7a648c
Esaú García Sánchez-Torija <egasato@protonmail.com>
2020-01-19 21:33:36 +0100

    Make Registration use contracts

--- src/es/upm/grise/profundizacion/contratos/registration/Registration.java
+++ src/es/upm/grise/profundizacion/contratos/registration/Registration.java
@@ -4,53 +4,44 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
+import com.google.java.contract.Ensures;
+
 import es.upm.grise.profundizacion.contratos.courses.Course;
 
 public class Registration {
-	
+
 	Set<Course> courses = new TreeSet<Course>();
-	
+
+	@Ensures("getNumberCourses() <= 10 &&" + "getTotalCredits() <= 36 &&" + "getRegistrationFee() >= 0")
 	public void addCourse(Course course) {
 		courses.add(course);
 	}
-	
+
 	public double getRegistrationFee() {
-		
 		double total = 0;
-		
-		for(Iterator<Course> it = courses.iterator(); it.hasNext();) {
+		for (Iterator<Course> it = courses.iterator(); it.hasNext(); ) {
 			Course tmp = it.next();
 			total += tmp.getFee();
 		}
-		
 		return total;
-		
 	}
-	
+
 	public int getTotalCredits() {
-		
 		int total = 0;
-		
-		for(Iterator<Course> it = courses.iterator(); it.hasNext();) {
+		for (Iterator<Course> it = courses.iterator(); it.hasNext(); ) {
 			Course tmp = it.next();
 			total += tmp.getCredits();
 		}
-		
 		return total;
-		
 	}
-	
+
 	public int getNumberCourses() {
-		
 		int total = 0;
-		
-		for(Iterator<Course> it = courses.iterator(); it.hasNext();) {
+		for (Iterator<Course> it = courses.iterator(); it.hasNext(); ) {
 			it.next();
 			total += 1;
 		}
-		
 		return total;
-		
 	}
 
 }
