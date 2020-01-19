dbdf68332cb8bbe1db200024e4dedfd4a75ecd0b
Adrian Rodriguez <ardguezsocorro@gmail.com>
2020-01-19 23:24:20 +0100

    ejercicio finalzado de contracts

--- changes.md
+++ changes.md
@@ -1,1 +1,8 @@
-# Document here the issues associated to items g) and h)
+Con respecto al apartado "g" no se puede dar una situación posible en la que el valor del importe una asignatura pueda ser negativo: 
+
+- Por una parte tenemos los grados donde el valor mínimo que se nos devolvera es 0, mientras que por otro lado tenemos los másters cuyo valor de curso se basa en "feePerCredit * courseData.getCredits() * numRegistrations" por ello para que este valor fuera negativo sería necesario que variasemos la constante feePercredit, o que los créditos o número de registros fuera negativo ( lo cual no es viable porque hemos establecido condiciones para que no pase esto.)
+
+- Una vez expuesto esto la solución pasaría por o modificar el código para las pruebas o crear algún método propio para que se diera esa situación.
+
+Con respecto al apartado "h" si no podemos generar importes negativos para asignaturas... no podemos dar esta situación tampoco ya que la matrícula es la suma de todos los precios de asignaturas.
+
--- test/es/upm/grise/profundizacion/contratos/CourseDataValuesTest.java
+++ test/es/upm/grise/profundizacion/contratos/CourseDataValuesTest.java
@@ -5,7 +5,27 @@ import es.upm.grise.profundizacion.contratos.values.CourseData;
 public enum CourseDataValuesTest implements CourseData {
 	
 	CORRECT_DEGREE_COURSE ("DEGREE", "Correct degree course", 5),
-	CORRECT_MASTER_COURSE ("MASTER", "Correct master course", 3);
+	CORRECT_MASTER_COURSE ("MASTER", "Correct master course", 3),
+	
+	DEGREE_WITH_ZERO_CREDITS("DEGREE", "Incorrect degree course",0),
+	MASTER_WITH_ZERO_CREDITS("MASTER","Incorrect master course", 0),
+	
+	DEGREE_WITH_SEVEN_CREDITS("DEGREE","Incorrect degree course",7),
+	MASTER_WITH_SEVEN_CREDITS("MASTER","Incorrect degree course",7),
+	
+	DEGREE_COURSE_N1 ("DEGREE", "Correct degree course 1", 2),
+	DEGREE_COURSE_N2 ("DEGREE", "Correct degree course 2", 2),
+	DEGREE_COURSE_N3 ("DEGREE", "Correct degree course 3", 2),
+	DEGREE_COURSE_N4 ("DEGREE", "Correct degree course 4", 2),
+	DEGREE_COURSE_N5 ("DEGREE", "Correct degree course 5", 2),
+	DEGREE_COURSE_N6 ("DEGREE", "Correct degree course 6", 2),
+	DEGREE_COURSE_N7 ("DEGREE", "Correct degree course 7", 2),
+	DEGREE_COURSE_N8 ("DEGREE", "Correct degree course 8", 4),
+	DEGREE_COURSE_N9 ("DEGREE", "Correct degree course 9", 4),
+	DEGREE_COURSE_N10 ("DEGREE", "Correct degree course 10", 2),
+	DEGREE_COURSE_N11 ("DEGREE", "Correct degree course 11", 6),
+	DEGREE_COURSE_N12 ("DEGREE", "Correct degree course 12", 6),
+	DEGREE_COURSE_N13 ("DEGREE", "Correct degree course 13", 6);
 	
 	private String level;
 	private String name;
