d94bf2d9faea33dd5d1189748e87085522f356e0
Esaú García Sánchez-Torija <egasato@protonmail.com>
2020-01-19 21:26:24 +0100

    Make MasterCourse use contracts

--- src/es/upm/grise/profundizacion/contratos/courses/MasterCourse.java
+++ src/es/upm/grise/profundizacion/contratos/courses/MasterCourse.java
@@ -1,20 +1,23 @@
 package es.upm.grise.profundizacion.contratos.courses;
 
+import com.google.java.contract.Ensures;
 import com.google.java.contract.Requires;
 
 import es.upm.grise.profundizacion.contratos.values.CourseData;
 
 public class MasterCourse extends Course {
-	
+
 	final private static double feePerCredit = 35.44;
 
+	@Ensures("getFee() >= 0")
+	@Requires("\"MASTER\".equals(course.getLevel()) &&" + "course.getCredits() >= 1 && course.getCredits() <= 6 &&" + "numRegistrations >= 1 && numRegistrations <= 6")
 	public MasterCourse(CourseData course, int numRegistrations) {
 		super(numRegistrations);
 		this.courseData = course;
 	}
-	
+
 	public double getFee() {
 		return feePerCredit * courseData.getCredits() * numRegistrations;
 	}
-	
+
 }
